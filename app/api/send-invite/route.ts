14fb913cf2bd7af94d2f978c31300193cf149a0f
Gabriel Scarabel <42478593+GScarabel@users.noreply.github.com>
2025-10-20 21:44:55 -0300

    correcao resend

--- app/api/send-invite/route.ts
+++ app/api/send-invite/route.ts
@@ -1,7 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend('re_jkXX9MXy_C2qpmdVGrJiRThSQAo1vBMoW');
+
 
 export async function POST(req: NextRequest) {
   try {
